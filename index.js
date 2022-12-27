9b5c0934b15a7d311b7c0185d78c245ead6e70d5
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-28 01:34:35 +0200

    step 3

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/test', (req, res) => {
+    res.json({ status: 200, message: 'ok' });
+});
+app.get('/time', (req, res) => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    res.json({ status: 200, message: `${hours}:${minutes}` });
+  });
 
 
-app.listen(3001, () => {
-    console.log('listening on port 3001');
+app.listen(3003, () => {
+    console.log('listening on port 3003');
 });
